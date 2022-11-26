effe06a15842253f1b5ff336f6265a46afa7c30b
Ares Koliadimas <ariskoliadimas@gmail.com>
2022-11-26 17:28:58 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'pop-movies',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
